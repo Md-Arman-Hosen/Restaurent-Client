3a0e36b11c25293b90663a4dd8fc2abc3a0ab7cb
MD-Arman-Hosen <arman15-3304@diu.edu.bd>
2025-02-27 05:33:17 +0600

    custom hook created for cart and tan stack query added

--- src/Components/SectionTitle/FoodCard/FoodCard.jsx
+++ src/Components/SectionTitle/FoodCard/FoodCard.jsx
@@ -3,13 +3,14 @@
 import Swal from "sweetalert2";
 import useAuth from "../../../Hooks/useAuth";
 import { useLocation, useNavigate } from "react-router-dom";
-import axios from "axios";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 
 const FoodCard = ({ item }) => {
     const { name, image, price, recipe, _id } = item;
     const { user } = useAuth();
     const navigate = useNavigate();
     const location = useLocation();
+    const axiosSecure = useAxiosSecure();
 
     const handleAddToCart = food => {
         if (user && user.email) {
@@ -22,7 +23,7 @@ const FoodCard = ({ item }) => {
                 image,
                 price
             }
-            axios.post('http://localhost:5000/carts', cartItem)
+            axiosSecure.post('/carts', cartItem)
                 .then(res => {
                     console.log(res.data)
                     if (res.data.insertedId) {
